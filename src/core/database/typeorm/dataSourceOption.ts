9b1236010307eeb60b470e446f6229d996fb322c
jeong <jw.jeong@tilda.co.kr>
2023-08-10 23:50:47 +0900

    database option 수정

--- src/core/database/typeorm/dataSourceOption.ts
+++ src/core/database/typeorm/dataSourceOption.ts
@@ -1,12 +1,13 @@
 import { ConfigService } from '@nestjs/config';
 import { DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 export function getDataSourceOptions(
   configService: ConfigService,
 ): DataSourceOptions {
   return {
     name: 'default',
-    type: 'mysql',
+    type: 'postgres',
     host: configService.get('RDS_HOSTNAME'),
     port: configService.get('RDS_PORT'),
     database: configService.get('RDS_DB_NAME'),
@@ -17,5 +18,6 @@ export function getDataSourceOptions(
     synchronize: false,
     logging: ['error', 'schema'],
     migrationsRun: true,
+    namingStrategy: new SnakeNamingStrategy(),
   };
 }
