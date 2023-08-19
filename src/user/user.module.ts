fad2863d4f5eee9027f2528e59398a1057952ab9
jawoon <jw.jeong@gitlab.co.kr>
2023-08-19 17:03:02 +0900

    유저 모듈에 의존성 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './adapter/in/web/user.controller';
 import { UserService } from './application/service/user.service';
+import { UserPersistenceAdapter } from './adapter/out/database/user-persistence.adapter';
+import { UserEntityMapper } from './adapter/out/database/user.entity-mapper';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './adapter/out/database/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserPersistenceAdapter, UserEntityMapper],
 })
 export class UserModule {}
